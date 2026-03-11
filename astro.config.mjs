7ba689045d87fd0a00a432dd782247b41ba6597c
GVPV12 <greciavalentinapv@gmail.com>
2026-03-11 15:45:26 -0400

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
 
-// https://astro.build/config
+// Borramos la línea de import netlify
+
 export default defineConfig({
-  site: 'https://GVPV12.githu.io',
-  base:'anywhere-healing',
-  output: "static", // Para Shopify
-  adapter: netlify(),
-  experimental: {
-    session: true // Esto soluciona el error
-  }
+  site: 'https://GVPV12.github.io',
+  base: '/anywhere-healing',
+  output: 'static', 
+  // Borramos adapter: netlify()
+  // Borramos experimental: { session: true } (GitHub Pages no soporta sesiones de servidor)
 });
